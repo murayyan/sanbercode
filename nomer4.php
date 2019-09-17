eb2cd716b426a4dbeaa36a18a4535357394ab36e
Muhammad Rayyan <murayyan97@gmail.com>
2019-09-17 18:29:40 +0700

    push gan

---
+++ nomer4.php
@@ -0,0 +1,18 @@
+<?php 
+
+$a =   ["1" =>[
+            "row3" => "field1 dengan field3 = S",
+            "row5" => "field1 dengan field3 = A"],
+        "2" =>
+            ["row1" => "field1 dengan field3 = S", 
+            "row2" => "field1 dengan field3 = A"],
+        "3" =>
+            ["row4" => "field1 dengan field3 = S"],
+        "4" =>
+            ["row6" => "field1 dengan field3 = A"],
+        "5" =>
+            ["row7" => "field1 dengan field3 = S",
+            "row8" => "field1 dengan field3 = A"]
+        ];
+ 
+print_r($a);
